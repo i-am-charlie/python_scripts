542c98dbc3f8ad0571156de1ba0c05167fd1c3a8
i.am.charlie <charlesv@rams.colostate.edu>
2014-01-02 10:09:08 -0600

    Added comments to script

--- fibo.py
+++ fibo.py
@@ -1,3 +1,8 @@
+""" This is a small program that calculates the first 'x' fibonacci numbers.
+	This was written to initialize a git repository of all of my exploratory
+	python scripts.
+	"""
+
 def fibo(x):
 	if x == 0:
 		return 0
